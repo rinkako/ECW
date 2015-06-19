fa4396f381ef57cda5cb78975b8b05bbe67c03ef
wumuyunren <sysu_linhs@163.com>
2015-06-19 17:04:45 +0800

    1

---
+++ ECWClient/PreDownPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ECWClient
+{
+    /// <summary>
+    /// PreDownPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class PreDownPage : Window
+    {
+        public PreDownPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_close_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                try
+                {
+                    this.DragMove();
+                }
+                catch (Exception ex)
+                {
+                    // HERE PREVENT STOP BY SUB WIN MOUSE
+                }
+            }
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
